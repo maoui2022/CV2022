ea25586c4a8fd84ae2f788fabc667015bf02f859
aziz <maoui7480@gmail.com>
2022-08-27 16:01:34 +0200

    Premier commit de mon portfolio

---
+++ port.js
@@ -0,0 +1,28 @@
+
+	
+		const kk = document.querySelector('.kk');
+       kk.classList.add('animate__animated', 'animate__rotateIn','animate__delay-2s','animate__repeat-2');
+	
+
+	
+		const element = document.querySelector('.xx');
+	    
+       element.classList.add('animate__animated', 'animate__rotateIn','animate__repeat-2');
+       element.style.setProperty('--animate-duration', '1s');
+
+	
+		const yy = document.querySelector('.yy');
+       yy.classList.add('animate__animated', 'animate__zoomInDown');
+       yy.style.setProperty('--animate-duration', '4s');
+	
+	
+	
+		const zz = document.querySelector('.zz');
+       zz.classList.add('animate__animated', 'animate__zoomInDown');
+       zz.style.setProperty('--animate-duration', '5s');
+	
+
+	
+		const zzz = document.querySelector('.zzz');
+       zzz.classList.add('animate__animated', 'animate__zoomInDown');
+       zzz.style.setProperty('--animate-duration', '6s');
